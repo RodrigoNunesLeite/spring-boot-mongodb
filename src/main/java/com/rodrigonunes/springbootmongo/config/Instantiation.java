e9c0226605e10eca3ac19b30725434ce145636fc
Rodrigo Nunes <rodrigonl10.rn@gmail.com>
2020-06-14 21:31:16 -0300

    Operacao de instanciacao da base de dados

---
+++ src/main/java/com/rodrigonunes/springbootmongo/config/Instantiation.java
@@ -0,0 +1,37 @@
+/*
+ * Esta classe serve para que assim que iniciar a aplicaçao,
+ * já cadastrar esses dados no banco
+ * */
+
+package com.rodrigonunes.springbootmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.rodrigonunes.springbootmongo.domain.User;
+import com.rodrigonunes.springbootmongo.repository.UserRepository;
+
+// @Configuration = para o spring entender que é uma configuração
+@Configuration
+public class Instantiation implements CommandLineRunner {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		// limpa a collection no mongoDB
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
+	
+}
