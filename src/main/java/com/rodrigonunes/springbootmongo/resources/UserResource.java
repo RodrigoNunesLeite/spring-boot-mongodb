0d26a8e50d0fccaa02f59db56af357418b4d3b82
Rodrigo Nunes <rodrigonl10.rn@gmail.com>
2020-06-14 21:42:42 -0300

    Usando padrao DTO para retornar usuarios

--- src/main/java/com/rodrigonunes/springbootmongo/resources/UserResource.java
+++ src/main/java/com/rodrigonunes/springbootmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.rodrigonunes.springbootmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rodrigonunes.springbootmongo.domain.User;
+import com.rodrigonunes.springbootmongo.dto.UserDTO;
 import com.rodrigonunes.springbootmongo.services.UserService;
 
 @RestController
@@ -19,8 +21,16 @@ public class UserResource {
 	private UserService service;
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		/*
+		 * Primeiro converto a lista para uma stream, a stream
+		 * é uma coleção que possibilita o uso de expressão lambda.
+		 * Através de expressao lambada, estou pegando cada elemento x
+		 * da lista list e instanciando um novo userDTO recebendo o objeto x,
+		 * depois convertendo para lista novamente.
+		 * */
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
